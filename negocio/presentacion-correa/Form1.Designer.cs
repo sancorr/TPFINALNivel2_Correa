ba9dcd456542afd6155f3d7079c07891fe4d3f6f
Santiago Correa <correasanti1997@gmail.com>
2024-10-21 23:43:45 -0300

    implementacion de cargarImagenLocal y GC

---
+++ negocio/presentacion-correa/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace presentacion_correa
+{
+	partial class formArticulos
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dgvArticulos = new System.Windows.Forms.DataGridView();
+			this.pbxArticulo = new System.Windows.Forms.PictureBox();
+			this.labelCampo = new System.Windows.Forms.Label();
+			this.labelCriterio = new System.Windows.Forms.Label();
+			this.labelFiltro = new System.Windows.Forms.Label();
+			this.cbxCampo = new System.Windows.Forms.ComboBox();
+			this.cbxCriterio = new System.Windows.Forms.ComboBox();
+			this.tbxFiltro = new System.Windows.Forms.TextBox();
+			this.btnBuscar = new System.Windows.Forms.Button();
+			this.btnNuevoArticulo = new System.Windows.Forms.Button();
+			this.btnModificarArticulo = new System.Windows.Forms.Button();
+			this.btnEliminar = new System.Windows.Forms.Button();
+			this.btnVerEliminados = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbxArticulo)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgvArticulos
+			// 
+			this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvArticulos.Location = new System.Drawing.Point(12, 129);
+			this.dgvArticulos.Name = "dgvArticulos";
+			this.dgvArticulos.Size = new System.Drawing.Size(796, 194);
+			this.dgvArticulos.TabIndex = 0;
+			this.dgvArticulos.SelectionChanged += new System.EventHandler(this.dgvArticulos_SelectionChanged);
+			// 
+			// pbxArticulo
+			// 
+			this.pbxArticulo.Location = new System.Drawing.Point(844, 129);
+			this.pbxArticulo.Name = "pbxArticulo";
+			this.pbxArticulo.Size = new System.Drawing.Size(213, 194);
+			this.pbxArticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pbxArticulo.TabIndex = 1;
+			this.pbxArticulo.TabStop = false;
+			// 
+			// labelCampo
+			// 
+			this.labelCampo.AutoSize = true;
+			this.labelCampo.Location = new System.Drawing.Point(21, 72);
+			this.labelCampo.Name = "labelCampo";
+			this.labelCampo.Size = new System.Drawing.Size(40, 13);
+			this.labelCampo.TabIndex = 2;
+			this.labelCampo.Text = "Campo";
+			// 
+			// labelCriterio
+			// 
+			this.labelCriterio.AutoSize = true;
+			this.labelCriterio.Location = new System.Drawing.Point(211, 72);
+			this.labelCriterio.Name = "labelCriterio";
+			this.labelCriterio.Size = new System.Drawing.Size(39, 13);
+			this.labelCriterio.TabIndex = 3;
+			this.labelCriterio.Text = "Criterio";
+			// 
+			// labelFiltro
+			// 
+			this.labelFiltro.AutoSize = true;
+			this.labelFiltro.Location = new System.Drawing.Point(401, 72);
+			this.labelFiltro.Name = "labelFiltro";
+			this.labelFiltro.Size = new System.Drawing.Size(29, 13);
+			this.labelFiltro.TabIndex = 4;
+			this.labelFiltro.Text = "Filtro";
+			// 
+			// cbxCampo
+			// 
+			this.cbxCampo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbxCampo.FormattingEnabled = true;
+			this.cbxCampo.Location = new System.Drawing.Point(73, 68);
+			this.cbxCampo.Name = "cbxCampo";
+			this.cbxCampo.Size = new System.Drawing.Size(121, 21);
+			this.cbxCampo.TabIndex = 5;
+			this.cbxCampo.SelectedIndexChanged += new System.EventHandler(this.cbxCampo_SelectedIndexChanged);
+			// 
+			// cbxCriterio
+			// 
+			this.cbxCriterio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbxCriterio.FormattingEnabled = true;
+			this.cbxCriterio.Location = new System.Drawing.Point(263, 68);
+			this.cbxCriterio.Name = "cbxCriterio";
+			this.cbxCriterio.Size = new System.Drawing.Size(121, 21);
+			this.cbxCriterio.TabIndex = 6;
+			// 
+			// tbxFiltro
+			// 
+			this.tbxFiltro.Location = new System.Drawing.Point(453, 68);
+			this.tbxFiltro.Name = "tbxFiltro";
+			this.tbxFiltro.Size = new System.Drawing.Size(137, 20);
+			this.tbxFiltro.TabIndex = 7;
+			// 
+			// btnBuscar
+			// 
+			this.btnBuscar.Location = new System.Drawing.Point(635, 66);
+			this.btnBuscar.Name = "btnBuscar";
+			this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+			this.btnBuscar.TabIndex = 8;
+			this.btnBuscar.Text = "Buscar";
+			this.btnBuscar.UseVisualStyleBackColor = true;
+			this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+			// 
+			// btnNuevoArticulo
+			// 
+			this.btnNuevoArticulo.Location = new System.Drawing.Point(12, 345);
+			this.btnNuevoArticulo.Name = "btnNuevoArticulo";
+			this.btnNuevoArticulo.Size = new System.Drawing.Size(87, 23);
+			this.btnNuevoArticulo.TabIndex = 9;
+			this.btnNuevoArticulo.Text = "Nuevo artículo";
+			this.btnNuevoArticulo.UseVisualStyleBackColor = true;
+			this.btnNuevoArticulo.Click += new System.EventHandler(this.btnNuevoArticulo_Click);
+			// 
+			// btnModificarArticulo
+			// 
+			this.btnModificarArticulo.Location = new System.Drawing.Point(123, 345);
+			this.btnModificarArticulo.Name = "btnModificarArticulo";
+			this.btnModificarArticulo.Size = new System.Drawing.Size(75, 23);
+			this.btnModificarArticulo.TabIndex = 10;
+			this.btnModificarArticulo.Text = "Modificar artículo";
+			this.btnModificarArticulo.UseVisualStyleBackColor = true;
+			this.btnModificarArticulo.Click += new System.EventHandler(this.btnModificarArticulo_Click);
+			// 
+			// btnEliminar
+			// 
+			this.btnEliminar.Location = new System.Drawing.Point(222, 345);
+			this.btnEliminar.Name = "btnEliminar";
+			this.btnEliminar.Size = new System.Drawing.Size(116, 23);
+			this.btnEliminar.TabIndex = 11;
+			this.btnEliminar.Text = "Eliminar artículo";
+			this.btnEliminar.UseVisualStyleBackColor = true;
+			this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+			// 
+			// btnVerEliminados
+			// 
+			this.btnVerEliminados.Location = new System.Drawing.Point(362, 345);
+			this.btnVerEliminados.Name = "btnVerEliminados";
+			this.btnVerEliminados.Size = new System.Drawing.Size(106, 23);
+			this.btnVerEliminados.TabIndex = 12;
+			this.btnVerEliminados.Text = "Ver eliminados";
+			this.btnVerEliminados.UseVisualStyleBackColor = true;
+			this.btnVerEliminados.Click += new System.EventHandler(this.btnVerEliminados_Click);
+			// 
+			// formArticulos
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1094, 450);
+			this.Controls.Add(this.btnVerEliminados);
+			this.Controls.Add(this.btnEliminar);
+			this.Controls.Add(this.btnModificarArticulo);
+			this.Controls.Add(this.btnNuevoArticulo);
+			this.Controls.Add(this.btnBuscar);
+			this.Controls.Add(this.tbxFiltro);
+			this.Controls.Add(this.cbxCriterio);
+			this.Controls.Add(this.cbxCampo);
+			this.Controls.Add(this.labelFiltro);
+			this.Controls.Add(this.labelCriterio);
+			this.Controls.Add(this.labelCampo);
+			this.Controls.Add(this.pbxArticulo);
+			this.Controls.Add(this.dgvArticulos);
+			this.Name = "formArticulos";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Articulos";
+			this.Load += new System.EventHandler(this.formArticulos_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbxArticulo)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dgvArticulos;
+		private System.Windows.Forms.Label labelCampo;
+		private System.Windows.Forms.Label labelCriterio;
+		private System.Windows.Forms.Label labelFiltro;
+		private System.Windows.Forms.ComboBox cbxCampo;
+		private System.Windows.Forms.ComboBox cbxCriterio;
+		private System.Windows.Forms.TextBox tbxFiltro;
+		private System.Windows.Forms.Button btnBuscar;
+		private System.Windows.Forms.Button btnNuevoArticulo;
+		private System.Windows.Forms.Button btnModificarArticulo;
+		private System.Windows.Forms.Button btnEliminar;
+		private System.Windows.Forms.Button btnVerEliminados;
+		public System.Windows.Forms.PictureBox pbxArticulo;
+	}
+}
+
---
+++ negocio/presentacion-correa/FormNuevoArticulo.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dominio;
+using negocio;
+using System.IO;
+using System.Configuration;
+
+namespace presentacion_correa
+{
+	public partial class FormNuevoArticulo : Form
+	{
+		private Articulo article = null;
+		private OpenFileDialog archivo = null;
+
+		//REF AL FORMULARIO PRINCIPAL
+		private formArticulos formPrincipal;
+
+		//boton agregar - constructor
+		public FormNuevoArticulo()
+		{
+			InitializeComponent();
+		}
+
+		//boton modificar - constructor
+		public FormNuevoArticulo(Articulo art, formArticulos formPrincipal)
+		{
+			InitializeComponent();
+			this.article = art;
+			//REF AL FORM PRINCIPAL QUE VIENE POR PARAMETRO
+			this.formPrincipal = formPrincipal;
+		}
+
+		private void btnCancelarNuevo_Click(object sender, EventArgs e)
+		{
+			DialogResult res = MessageBox.Show("¿Seguro desea cancelar? Los cambios no se guardaran", "CANCELAR", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+			if (res == DialogResult.Yes)
+			{
+				this.Close();
+			}
+
+		}
+
+		private void btnAgregarNuevo_Click(object sender, EventArgs e)
+		{
+			ArticuloNegocio negocio = new ArticuloNegocio();
+
+			if(article == null)
+				article = new Articulo();
+
+			try
+			{
+				//Si el articulo existe, me guardo la URL de su imagen
+				string imagenAnterior = null;
+				if (article.Id != 0)
+				{
+					imagenAnterior = article.ImagenUrl;
+				}
+				
+
+				article.Codigo = tbxCodigoNuevo.Text;
+				article.Nombre = tbxNombreNuevo.Text;
+				article.Descripcion = tbxDescripcionNuevo.Text;
+				article.Categoria = (CategoriaArticulo)cbxCategoriaNuevo.SelectedItem;
+				article.Marca = (MarcaArticulo)cbxMarcaNuevo.SelectedItem;
+				//article.ImagenUrl = tbxUrlNuevo.Text;
+				article.Precio = decimal.Parse(tbxPrecioNuevo.Text);
+
+				//SI ES UNA MODIFICACION
+				if(article.Id != 0)
+				{
+
+					if (archivo != null && !(tbxUrlNuevo.Text.ToLower().Contains("http")))
+					{
+						string destinoImagen = ConfigurationManager.AppSettings["images-folder"] + archivo.SafeFileName;
+
+						if (formPrincipal.pbxArticulo.ImageLocation == imagenAnterior)
+						{
+								formPrincipal.pbxArticulo.Image.Dispose();
+								formPrincipal.pbxArticulo.Image = null;
+						}
+
+						File.Copy(archivo.FileName, destinoImagen);
+						article.ImagenUrl = destinoImagen;
+						
+					}
+					else
+					{
+						article.ImagenUrl = tbxUrlNuevo.Text;
+					}
+
+					negocio.modificarArticulo(article);
+
+					formPrincipal.pbxArticulo.ImageLocation = article.ImagenUrl;
+
+					if (File.Exists(imagenAnterior))
+					{
+						try
+						{
+							System.GC.Collect();
+							System.GC.WaitForPendingFinalizers();
+							
+							File.Delete(imagenAnterior);
+						}
+						catch (Exception ex)
+						{
+							MessageBox.Show("Error al eliminar la imagen anterior: " + ex.ToString());
+						}
+					}
+
+
+
+					MessageBox.Show("Articulo modificado con éxito", "ARTICULO MODIFICADO");
+			}
+				else
+				{
+					
+					if (archivo != null && !(tbxUrlNuevo.Text.ToLower().Contains("http")))
+					{
+						string destinoImagen = ConfigurationManager.AppSettings["images-folder"] + archivo.SafeFileName;
+
+						File.Copy(archivo.FileName, destinoImagen);
+						article.ImagenUrl = destinoImagen;
+					}
+					else
+					{
+						article.ImagenUrl = tbxUrlNuevo.Text;
+					}
+
+					negocio.agregarArticulo(article);
+					MessageBox.Show("Articulo agregado con éxito", "ARTICULO AGREGADO");
+				}
+
+				Close();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+
+		private void FormNuevoArticulo_Load(object sender, EventArgs e)
+		{
+			CategoriaArticuloNegocio articuloNuevoNegocio = new CategoriaArticuloNegocio();
+			MarcaArticuloNegocio marcaArticuloNegocio = new MarcaArticuloNegocio();
+			try
+			{
+				cbxMarcaNuevo.DataSource = marcaArticuloNegocio.listar();
+				cbxMarcaNuevo.ValueMember = "Id";
+				cbxMarcaNuevo.DisplayMember = "Descripcion";
+				cbxCategoriaNuevo.DataSource = articuloNuevoNegocio.listar();
+				cbxCategoriaNuevo.ValueMember = "Id";
+				cbxCategoriaNuevo.DisplayMember = "Descripcion";
+
+				if (article != null)
+				{
+					tbxCodigoNuevo.Text = article.Codigo;
+					tbxNombreNuevo.Text = article.Nombre;
+					tbxDescripcionNuevo.Text = article.Descripcion;
+
+					cbxMarcaNuevo.SelectedValue = article.Marca.Id;
+					cbxCategoriaNuevo.SelectedValue = article.Categoria.Id;					
+
+					tbxUrlNuevo.Text = article.ImagenUrl;
+					cargarImagen(article.ImagenUrl);
+					tbxPrecioNuevo.Text = article.Precio.ToString();
+
+				}
+			}
+			catch (Exception ex)
+			{
+
+				MessageBox.Show(ex.ToString());
+			}
+		}
+
+		private void cargarImagenLocal(string imagen)
+		{
+			try
+			{
+				using (var stream = new MemoryStream(File.ReadAllBytes(imagen)))
+				{
+					pbxNuevoArticulo.Image = Image.FromStream(stream);
+				}
+			}
+			catch (Exception ex)
+			{
+
+				pbxNuevoArticulo.Load("https://imgs.search.brave.com/fVrzTsY8XbfClD6SD9ps0BmYFUEi7I2qsepvPy4Ypj4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA3LzU2LzY3LzM0/LzM2MF9GXzc1NjY3/MzQ2Nl9RclpHNU45/bDM4TGw4cE1NQW5J/NzgwWWxQcVROMm5h/aC5qcGc");
+			}
+		}
+		
+		private void cargarImagen(string imagen)
+		{
+			try
+			{
+				pbxNuevoArticulo.Load(imagen);
+
+			}
+			catch (Exception ex)
+			{
+
+				pbxNuevoArticulo.Load("https://imgs.search.brave.com/fVrzTsY8XbfClD6SD9ps0BmYFUEi7I2qsepvPy4Ypj4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA3LzU2LzY3LzM0/LzM2MF9GXzc1NjY3/MzQ2Nl9RclpHNU45/bDM4TGw4cE1NQW5J/NzgwWWxQcVROMm5h/aC5qcGc");
+			}
+		}
+
+		private void tbxUrlNuevo_Leave(object sender, EventArgs e)
+		{
+			cargarImagen(tbxUrlNuevo.Text);
+		}
+
+		private void btnImagen_Click(object sender, EventArgs e)
+		{
+			archivo = new OpenFileDialog();
+			archivo.Filter = "jpg|*.jpg;| png|*.png";
+			if(archivo.ShowDialog() == DialogResult.OK)
+			{
+				if(article != null && article.Id != 0)
+				{
+					if(pbxNuevoArticulo.Image != null)
+					{
+						pbxNuevoArticulo.Dispose();
+						pbxNuevoArticulo.Image = null;
+					}
+				}
+				tbxUrlNuevo.Text = archivo.FileName;
+				cargarImagenLocal(archivo.FileName);
+
+			}
+		}
+	}
+}
